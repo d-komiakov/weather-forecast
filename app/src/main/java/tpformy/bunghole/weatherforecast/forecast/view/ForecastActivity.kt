6315b041553fd1fa31595c94ca79ce60229756b7
Dmitrii Komiakov <tarhunhansolo@gmail.com>
2019-01-14 02:11:33 +0500

    activity's embryo

---
+++ app/src/main/java/tpformy/bunghole/weatherforecast/forecast/view/ForecastActivity.kt
@@ -0,0 +1,13 @@
+package tpformy.bunghole.weatherforecast.forecast.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import tpformy.bunghole.weatherforecast.R
+
+class ForecastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast);
+    }
+}
