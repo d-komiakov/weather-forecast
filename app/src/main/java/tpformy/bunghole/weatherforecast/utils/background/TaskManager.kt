f3aa1d164c36c04bb65bdb4577143c0322ea3cfc
Dmitrii Komiakov <tarhunhansolo@gmail.com>
2019-01-17 01:00:09 +0500

    TaskManager - background tasks runner & processor

---
+++ app/src/main/java/tpformy/bunghole/weatherforecast/utils/background/TaskManager.kt
@@ -0,0 +1,29 @@
+package tpformy.bunghole.weatherforecast.utils.background
+
+import android.os.Handler
+
+interface TaskManager {
+    fun <TResult> runInBackground(backgroundAction: () -> TResult,
+                                  uiCallback: (TResult) -> Unit,
+                                  errorUiCallback: (Throwable) -> Unit)
+}
+
+class AndroidTaskManager(private val backgroundHandler: Handler, private val mainHandler: Handler) : TaskManager {
+    override fun <TResult> runInBackground(backgroundAction: () -> TResult,
+                                           uiCallback: (TResult) -> Unit,
+                                           errorUiCallback: (Throwable) -> Unit) {
+        backgroundHandler.run {
+            try {
+                val result = backgroundAction()
+                mainHandler.run {
+                    uiCallback(result)
+                }
+            } catch (t: Throwable) {
+                mainHandler.run {
+                    errorUiCallback(t)
+                }
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/tpformy/bunghole/weatherforecast/utils/background/TaskManagerFactory.kt
@@ -0,0 +1,23 @@
+package tpformy.bunghole.weatherforecast.utils.background
+
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+
+object TaskManagerFactory {
+    lateinit var backgroundHandler: Handler
+    lateinit var mainHandler: Handler
+
+    fun createTaskManager(): TaskManager {
+        if (!TaskManagerFactory::backgroundHandler.isInitialized) {
+            val backgroundHandlerThread = HandlerThread("TaskManagerThread")
+            backgroundHandlerThread.start()
+            backgroundHandler = Handler(backgroundHandlerThread.looper)
+        }
+
+        if (!TaskManagerFactory::mainHandler.isInitialized) {
+            mainHandler = Handler(Looper.getMainLooper())
+        }
+        return AndroidTaskManager(backgroundHandler, mainHandler)
+    }
+}
