a8181895303898f5ffc17eacb3495ac186a18985
Dmitrii Komiakov <tarhunhansolo@gmail.com>
2019-01-17 01:28:49 +0500

    TaskManager refactoring: runInBackground -> runTask

--- app/src/main/java/tpformy/bunghole/weatherforecast/utils/background/TaskManager.kt
+++ app/src/main/java/tpformy/bunghole/weatherforecast/utils/background/TaskManager.kt
@@ -3,15 +3,15 @@ package tpformy.bunghole.weatherforecast.utils.background
 import android.os.Handler
 
 interface TaskManager {
-    fun <TResult> runInBackground(backgroundAction: () -> TResult,
-                                  uiCallback: (TResult) -> Unit,
-                                  errorUiCallback: (Throwable) -> Unit)
+    fun <TResult> runTask(backgroundAction: () -> TResult,
+                          uiCallback: (TResult) -> Unit,
+                          errorUiCallback: (Throwable) -> Unit)
 }
 
 class AndroidTaskManager(private val backgroundHandler: Handler, private val mainHandler: Handler) : TaskManager {
-    override fun <TResult> runInBackground(backgroundAction: () -> TResult,
-                                           uiCallback: (TResult) -> Unit,
-                                           errorUiCallback: (Throwable) -> Unit) {
+    override fun <TResult> runTask(backgroundAction: () -> TResult,
+                                   uiCallback: (TResult) -> Unit,
+                                   errorUiCallback: (Throwable) -> Unit) {
         backgroundHandler.run {
             try {
                 val result = backgroundAction()
