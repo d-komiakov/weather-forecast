d8471bab64cbd2339423c888e87e7ede5bd07721
dmitrii komiakov <tarhunhansolo@gmail.com>
2019-10-13 21:08:38 +0300

    androidx migration

--- app/src/main/java/tpformy/bunghole/weatherforecast/forecast/view/ForecastActivity.kt
+++ app/src/main/java/tpformy/bunghole/weatherforecast/forecast/view/ForecastActivity.kt
@@ -2,7 +2,7 @@ package tpformy.bunghole.weatherforecast.forecast.view
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_forecast.*
