5cb5546072d6e92eff82f27b5aebf9678d28141c
dmitrii komiakov <tarhunhansolo@gmail.com>
2019-10-14 08:34:54 +0300

    AndroidTaskManager extracted

---
+++ app/src/main/java/tpformy/bunghole/weatherforecast/utils/task/AndroidTaskManager.kt
@@ -0,0 +1,24 @@
+package tpformy.bunghole.weatherforecast.utils.task
+
+import android.os.Handler
+import java.util.concurrent.Executor
+
+class AndroidTaskManager(private val executor: Executor, private val mainHandler: Handler) :
+    TaskManager {
+    override fun <TResult> runTask(backgroundAction: () -> TResult,
+                                   uiCallback: (TResult) -> Unit,
+                                   errorUiCallback: (Throwable) -> Unit) {
+        executor.execute {
+            try {
+                val result = backgroundAction()
+                mainHandler.post {
+                    uiCallback(result)
+                }
+            } catch (t: Throwable) {
+                mainHandler.post {
+                    errorUiCallback(t)
+                }
+            }
+        }
+    }
+}
